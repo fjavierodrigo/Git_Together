7afa3ca2ae40f0b1819d314f3bbfb029bb9904c0
FJavier <fjavierodrigo@gmail.com>
2026-04-02 21:01:37 +0200

    Guardando cambios para meterlos a main

--- gittogether-backend/src/main/java/gittogether/tfg/AppGitTogether.java
+++ gittogether-backend/src/main/java/gittogether/tfg/AppGitTogether.java
@@ -3,8 +3,6 @@ package gittogether.tfg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import gittogether.tfg.entities.Usuario;
-
 @SpringBootApplication
 public class AppGitTogether {
 
